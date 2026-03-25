7cafea8a2eaafc83b94f3a12a0b164850667e2a0
Phantom <jmore-s010@chusd.org>
2026-03-25 13:46:18 -0700

    Delete ActualPortfoilo directory

--- ActualPortfoilo/Java/Javacript.js
+++
@@ -1,5 +0,0 @@
-if (!sessionStorage.getItem('alerted'))
-    {
-        window.alert('Welcome to my website. I know, this is supposed to be a portfoilo, though I kinda went overboard and wanted to make an actual website. This is a website from a fantasy world in my head, but I might make it real, change it a litte. I think it could be movie material since I have been trying to make it semi-realistic and also it uses sci-fi. Anyways, sorry for the long message, I will let you go now.')
-        sessionStorage.setItem('alerted', 'true');
-    }
